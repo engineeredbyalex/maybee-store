84f4ef852482695ce4c87f21c568c93158f53428
engineeredbyalex <nonstopzen26@gmail.com>
2024-01-25 12:36:52 +0200

    25.01 rev-4

--- components/Basic/MainButton.js
+++ components/Basic/MainButton.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { ButtonStyle } from "@/components/Basic/Button";
-import { CartContext } from "@/components/cart/CartContext";
+import { CartContext } from "../cart/CartContext";
 import { useContext } from "react";
 
 const FlyingButtonWrapper = styled.div`
--- components/Basic/navigation/Banner.js
+++ components/Basic/navigation/Banner.js
@@ -1,17 +1,14 @@
-import styled from "styled-components"
-import Wrapper from "../../layout/Wrapper"
-import Center from "../../layout/Center"
 
 
 export default function Banner() {
     return (
         <div className="absolute top-0 h-[80px] lg:h-[50px] flex items-center justify-center w-full bg-red-600">
-            <Wrapper>
-                <Center>
+            <div>
+                <div>
                     {/* <p className="text-white text-[14px] lg:text-[16px] text-center  uppercase">Utilizează codul <b>(MB10)</b> până pe 15 ianuarie 2024 pentru a obține <b>10% reducere</b>.</p> */}
                     <p className="text-white text-center  uppercase">CREAT CU GRIJĂ Și atenție - MAYBEE</p>
-                </Center>
-            </Wrapper>
+                </div>
+            </div>
         </div>
     )
 }
