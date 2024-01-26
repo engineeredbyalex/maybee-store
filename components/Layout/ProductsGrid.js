b195e164acea48928952b2e6a1339fb7cafe29d8
engineeredbyalex <nonstopzen26@gmail.com>
2024-01-27 01:28:01 +0200

    27.01 rev-2

--- components/Layout/ProductsGrid.js
+++ components/Layout/ProductsGrid.js
@@ -5,7 +5,7 @@ export default function ProductsGrid({ products, wishedProducts = [] }) {
   return (
     <div className="grid grid-cols-1 gap-[50px] lg:grid-cols-3">
       {products?.map((product, index) => (
-        <ProductBox  {...product} wished={wishedProducts.includes(product._id)} />
+        <ProductBox key={product._id}  {...product} wished={wishedProducts.includes(product._id)} />
       ))}
     </div>
   );
