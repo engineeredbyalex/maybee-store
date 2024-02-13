39eacaf412c948ce7198308f68638cbd5320e464
engineeredbyalex <nonstopzen26@gmail.com>
2024-02-13 19:55:58 +0200

    13.02 rev-3

--- components/icons/Bars.js
+++ components/icons/Bars.js
@@ -1,4 +1,4 @@
-function Bars() {
+export default function Bars() {
   return (
     <div className="w-[3rem] h-[2rem] flex flex-col items-center justify-between">
       <div className="w-[3rem] h-[0.35rem] rounded-full bg-[#595959]"></div>
@@ -8,4 +8,3 @@ function Bars() {
   )
 }
 
-export default Bars
