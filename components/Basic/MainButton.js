c71bcffbae5287b413e364c871eeffc223d4986e
engineeredbyalex <nonstopzen26@gmail.com>
2024-02-13 19:53:34 +0200

    13.02 rev-2

--- components/Basic/MainButton.js
+++ components/Basic/MainButton.js
@@ -5,8 +5,8 @@ export default function FlyingButton(props) {
   const { addProduct } = useContext(CartContext);
 
   return (
-    <div className={`inline-block ${props.disabled ? 'pointer-events-none' : 'pointer-events-auto'} ${props.main ? 'bg-[#595959] border-none text-white' : 'border-1 border-[#595959] text-[#595959]'} ${props.red ? 'bg-[#870000] border-1 border-[#870000] text-[#870000]' : ''} ${props.white ? 'bg-white border-3 border-white font-semibold' : ''} ${props.hover ? 'hover:rounded-full' : ''} m-10`}>
-      <button className="flex items-center justify-center h-12 w-48 rounded-md" onClick={() => addProduct(props._id, props.selectedValues)}>
+    <div className={`${props.disabled ? 'pointer-events-none' : 'pointer-events-auto'}`}>
+      <button className="flex items-center justify-center min-w-[15rem] max-h-[10rem]  rounded-md bg-[#595959] py-2" onClick={() => addProduct(props._id, props.selectedValues)}>
         {props.children}
       </button>
     </div>
