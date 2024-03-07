72d3c3912eb3bdedf5ea9c2e8636ddfe87bbd14d
engineeredbyalex <dev.lazarescu.alexandru@gmail.com>
2024-03-07 10:32:08 +0200

    07.03 rev-1

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import { CartContextProvider } from "../components/Cart/CartContext"
 import { SessionProvider } from "next-auth/react";
 import '../styles/global.css'
+import { Analytics } from "@vercel/analytics/react"
 
 import AuthProvider from "./providers";
 
 export default function App({ Component, pageProps: {  ...pageProps } }) {
   return (
     <>
+      <Analytics />
       <AuthProvider>
         <link rel="icon" href="" sizes="any" />
         <CartContextProvider>
