4baf9d6996dd07a68153f8fd3a8d4ced92536aa8
engineeredbyalex <dev.lazarescu.alexandru@gmail.com>
2024-06-07 18:42:04 +0300

    07.06 rev-5

--- components/Basic/Header.js
+++ components/Basic/Header.js
@@ -13,6 +13,10 @@ export default function Header() {
   const navigationBarRef = useRef(null);
   const { cartProducts } = useContext(CartContext);
 
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
   useEffect(() => {
     const navigationBar = navigationBarRef.current;
 
@@ -30,24 +34,32 @@ export default function Header() {
         document.body.style.overflowX = 'hidden';  // Enable scrolling
       }
     }
+
+    return () => {
+      document.body.style.overflow = '';  // Cleanup on component unmount or toggle change
+    };
   }, [toggle]);
 
   useEffect(() => {
-    window.addEventListener("resize", () => {
+    const handleResize = () => {
       if (window.innerWidth < 768) {
         setToggle(false);
+        document.body.style.overflowX = 'hidden';  // Ensure scrolling is enabled on resize
       }
-    });
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
     <div>
       <div ref={navigationBarRef} className="navigation_bar">
-        <div className="w-full h-[4rem] absolute top-0 flex items-center justify-center ">
+        <div className="w-full h-[4rem] absolute top-0 flex items-center justify-center">
           <div className="w-1/2 lg:w-1/3 flex items-center justify-center">
             <LogoSvg className="w-[3rem] md:w-[4rem] lg:w-[3.5rem] h-[3rem] md:h-[4rem] lg:h-[3.5rem] svg_icon" />
           </div>
-          <div className={toggle ? "w-screen h-auto absolute top-[50vh] bottom-0 left-0 right-0 text-[#fff] uppercase flex flex-col items-center justify-center z-[5]" : "gap-3 w-1/2 text-[#000] uppercase font-medium hidden lg:flex items-center justify-center "}>
+          <div className={toggle ? "w-screen h-auto absolute top-[50vh] bottom-0 left-0 right-0 text-[#fff] uppercase flex flex-col items-center justify-center z-[5]" : "gap-3 w-1/2 text-[#000] uppercase font-medium hidden lg:flex items-center justify-center"}>
             <Link href="/"><p className="font-[400] text-xl link-item">Acasă</p></Link>
             <Link href="/products"><p className="font-[400] text-xl link-item">Produse</p></Link>
             <Link href="/categories"><p className="font-[400] text-xl link-item">Catalog</p></Link>
@@ -64,7 +76,7 @@ export default function Header() {
             <Link className="" href="/search">
               <Search width="35" height="45" className="svg_icon" />
             </Link>
-            <Menu width="35" height="45" className="svg_icon cursor-pointer lg:hidden" onClick={() => setToggle(!toggle)} />
+            <Menu width="35" height="45" className="svg_icon cursor-pointer lg:hidden" onClick={handleToggle} />
           </div>
         </div>
       </div>
