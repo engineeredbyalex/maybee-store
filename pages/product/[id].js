dd4559a81540f0a30530ee5f507a90c62031312f
engineeredbyalex <dev.lazarescu.alexandru@gmail.com>
2024-07-05 16:26:04 +0300

    testing

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -31,32 +31,34 @@ export default function ProductPage({ product }) {
       <Header />
       <div className="py-20 min-h-screen">
         <Layout>
-          <div className="flex flex-col justify-start items-center w-full md:w-1/2">
-            <ProductImages images={product.images} />
-            <div className="w-full h-auto gap-4 mt-4 flex flex-col">
+          <div className="flex flex-col lg:flex-row gap-10 w-full">
+            <div className="w-full lg:w-1/2 flex justify-center">
+              <ProductImages images={product.images} />
+            </div>
+            <div className="w-full lg:w-1/2 flex flex-col gap-6">
               <div className="flex flex-col gap-2">
-                <p className="font-medium text-left text-black">{product.title}</p>
-                <p className="font-medium text-left text-black">{product.price} RON</p>
+                <h4 className=" text-black font-semibold ">{product.title}</h4>
+                <h5 className="font-medium text-black">{product.price} RON</h5>
               </div>
-              <div className="flex flex-row gap-5">
+              <div className="flex flex-col gap-5">
                 {product.properties?.map((property, index) => (
                   <div key={index} className="flex flex-col gap-2">
-                    <h5 className="text-lg font-bold uppercase text-[#000]">{property.name}</h5>
+                    <h5 className="font-semibold uppercase text-[#000]">{property.name}</h5>
                     <div className="flex flex-wrap gap-3">
                       {property.values?.map((value, valueIndex) => (
                         <div
                           key={valueIndex}
                           onClick={() => handlePropertyValueChange(property.name, value)}
-                          className={`px-3 py-2 w-auto text-center rounded-md cursor-pointer transition-colors duration-200 ${selectedValues[property.name] === value ? 'bg-[#7F1515] text-white' : 'bg-[#d3d3d3] text-[#000000]'}`}
+                          className={`w-full min-h-[3rem] px-3 py-2 flex items-center justify-center text-center rounded-md cursor-pointer transition-colors duration-200 ${selectedValues[property.name] === value ? 'bg-[#7F1515] text-white' : 'bg-[#d3d3d3] text-[#000000]'}`}
                         >
-                          <p className="text-sm">{value}</p>
+                          <p>{value}</p>
                         </div>
                       ))}
                     </div>
                   </div>
                 ))}
               </div>
-              <div className="w-[100%] mt-4 flex items-center justify-center">
+              <div className="w-full mt-4 flex items-center justify-center">
                 <FlyingButton
                   main
                   _id={product._id}
@@ -64,13 +66,13 @@ export default function ProductPage({ product }) {
                   selectedValues={selectedValues}
                   src={product.images?.[0]}
                 >
-                  <h6 className=" text-white font-normal">Adaugă în coș</h6>
+                  <h6 className="text-white font-normal">Adaugă în coș</h6>
                 </FlyingButton>
               </div>
             </div>
           </div>
         </Layout>
-        </div>
+      </div>
       <Footer />
     </div>
   );
