9c9e9cef80e68bdb6324ab12c8ab9959704fffea
engineeredbyalex <dev.lazarescu.alexandru@gmail.com>
2024-07-05 15:37:37 +0300

    testing

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -33,8 +33,29 @@ export default function ProductPage({ product }) {
         <Layout>
           <div className="flex flex-col justify-start items-center w-full md:w-1/2">
             <ProductImages images={product.images} />
-            <div className="w-full h-auto mt-4 flex flex-col">
-              <p className="font-medium text-left text-black">{product.title}</p>
+            <div className="w-full h-auto gap-4 mt-4 flex flex-col">
+              <div className="flex flex-col gap-2">
+                <p className="font-medium text-left text-black">{product.title}</p>
+                <p className="font-medium text-left text-black">{product.price} RON</p>
+              </div>
+              <div className="flex flex-row gap-5">
+                {product.properties?.map((property, index) => (
+                  <div key={index} className="flex flex-col gap-2">
+                    <h5 className="text-lg font-bold uppercase text-[#000]">{property.name}</h5>
+                    <div className="flex flex-wrap gap-3">
+                      {property.values?.map((value, valueIndex) => (
+                        <div
+                          key={valueIndex}
+                          onClick={() => handlePropertyValueChange(property.name, value)}
+                          className={`px-3 py-2 w-auto text-center rounded-md cursor-pointer transition-colors duration-200 ${selectedValues[property.name] === value ? 'bg-[#7F1515] text-white' : 'bg-[#d3d3d3] text-[#000000]'}`}
+                        >
+                          <p className="text-sm">{value}</p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
               <div className="w-[100%] mt-4 flex items-center justify-center">
                 <FlyingButton
                   main
@@ -43,7 +64,7 @@ export default function ProductPage({ product }) {
                   selectedValues={selectedValues}
                   src={product.images?.[0]}
                 >
-                  <p className=" text-white font-normal">Adaugă în coș</p>
+                  <h6 className=" text-white font-normal">Adaugă în coș</h6>
                 </FlyingButton>
               </div>
             </div>
