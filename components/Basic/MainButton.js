74cf7f3449e28d9ff308a96467f28597145a8042
engineeredbyalex <dev.lazarescu.alexandru@gmail.com>
2024-07-05 11:29:55 +0300

    testing

--- components/Basic/MainButton.js
+++ components/Basic/MainButton.js
@@ -1,21 +1,38 @@
 import { CartContext } from "../Cart/CartContext";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import Cart from "../../public/images/Cart.svg";
+import { gsap } from "gsap";
 
 export default function FlyingButton(props) {
   const { addProduct } = useContext(CartContext);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    if (toggle) {
+      gsap.to(".cartButton", { backgroundColor: "#33B249", duration: 0.5 });
+      // gsap.to(".cartButton", { backgroundColor: "#000", delay: 1, duration: 0.5 });
+    } else {
+      gsap.to(".cartButton", { backgroundColor: "#000", duration: 0.5 });
+    }
+  }, [toggle]);
+
+  const handleClick = () => {
+    addProduct(props._id, props.selectedValues);
+    setToggle(true);
+    setTimeout(() => setToggle(false), 1000); // Reset toggle after 1.5 seconds
+  };
 
   return (
     <div className={`${props.disabled ? 'pointer-events-none' : 'pointer-events-auto'} w-full`}>
       <button
-        className="w-full h-12 rounded-md  py-2 px-3 bg-[#000] flex items-center justify-center text-white"
-        onClick={() => addProduct(props._id, props.selectedValues)}
+        className="cartButton w-full flex items-center justify-center bg-black text-white px-4 py-2 rounded-md transition-colors duration-500"
+        onClick={handleClick}
         disabled={props.disabled}
       >
-        <div className="w-2/3">
-          {props.children}  
+        <div className="w-2/3 flex items-center justify-center">
+          {props.children}
         </div>
-        <Cart />
+        <Cart className="ml-2" />
       </button>
     </div>
   );
